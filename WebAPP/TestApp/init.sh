0892454d5bf2e1b3ecd506a671f62afc82015f7b
Makoto ISHIHARA <evakichi@be.wakwak.com>
2025-03-09 12:50:16 +0900

    WebAPPのTestApp化。

--- WebAPP/TestApp/init.sh
+++ WebAPP/TestApp/init.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-if [ ! -f ../.env ];then
+if [ ! -f ../../.env ];then
 	echo "please spacify ../../.env file."
 	exit 255
 fi
 
-. ../.env 
+. ../../.env 
 
-if [ ! -f ../.password ]; then
+if [ ! -f ../../.password ]; then
 	echo "cannot read ../../.password"
 	exit 254
 fi
 
-. ../.password
+. ../../.password
 
 rm -f conf/nginx.conf
 
@@ -24,9 +24,9 @@ EOF
 
 sed "s/__DOMAIN_NAME__/${DOMAIN_NAME}/g" ./Dockerfile.org > Dockerfile
 sed "s/__DOMAIN_NAME__/${DOMAIN_NAME}/g" conf/nginx.conf.org > conf/nginx.conf
+sed "s/__DOMAIN_NAME__/${DOMAIN_NAME}/g" application.properties.org > application.properties
 sed "s/__WEBAPP_CLIENT_ID__/${WEBAPP_CLIENT_ID}/g" -i application.properties
 sed "s/__WEBAPP_CLIENT_SECRET__/${WEBAPP_CLIENT_SECRET}/g" -i application.properties
-
 
 COMPOSE_BAKE=true docker compose up -d --build
 
