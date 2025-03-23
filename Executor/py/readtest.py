2d2c2c90d12db396bdb254515ffb68a0e1303c76
Makoto ISHIHARA <evakichi@be.wakwak.com>
2025-03-23 15:45:11 +0900

    Ignore data directory.

--- Executor/py/readtest.py
+++ Executor/py/readtest.py
@@ -2,6 +2,6 @@ from  pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import DoubleType, FloatType, LongType, StructType,StructField, StringType
 
 spark = SparkSession.builder.getOrCreate()
-df = spark.table("demo.nyc.taxis00002")
+df = spark.table("demo.nyc.taxis00003")
 df.show()
 print(df.schema)
